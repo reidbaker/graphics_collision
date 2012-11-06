cd8b03b20f43c5cdb8d2bdef3258ec44830cac76
Reid Baker <hamiltonbaker@yahoo.com>
2012-11-06 11:39:57 -0500

    collisions?

--- src/ParticleGenerator.java
+++ src/ParticleGenerator.java
@@ -5,17 +5,17 @@ import processing.core.PApplet;
 
 
 public class ParticleGenerator  implements Widget, MouseMotionListener  {
-	
+
 	private static final int MAX_PARTICLES = 5000;
 	private ArrayList<Particle> particles = new ArrayList<Particle>();
 	private Curve curve;
 
 	private Point pos;
 	private float radius;
-	
+
 	private int t;
 	private World w;
-	
+
 	public ParticleGenerator(Point p, Curve c, int r) {
 		pos = p;
 		radius = r;
@@ -26,17 +26,17 @@ public class ParticleGenerator  implements Widget, MouseMotionListener  {
 	@Override
 	public void draw(PApplet c) {
         w.draw(c);
-		
+
 		c.pushMatrix();
 		c.translate(pos.x,  pos.y, pos.z);
 		c.sphere(radius);
 		c.popMatrix();
-		
+
 		for (int i = particles.size() - 1; i >= 0; i--) {
-			
+
 			Particle p = particles.get(i);
 			Point closest = curve.getClosestPoint(p.pos, p.index);
-		    
+
 		    if (closest != null) {
 		    	p.index = curve.findPoint(closest);
 		    	p.velocity = curve.getTangent(closest);
@@ -46,37 +46,32 @@ public class ParticleGenerator  implements Widget, MouseMotionListener  {
 		    	if (curve.isLastPoint(closest)) {
 		    		particles.remove(p);
 		    	}
-		    	
+
 		    	for (Particle q: particles) {
 		    		if (!q.equals(p)) {
 		                //p.velocity = Point.add(p.velocity, Particle.force(p, q));
 		                float t = 1/GraphicsCollision.getInstance().frameRate;
-		    			while (t > 0) {
-		    				float m = Particle.collision(p,q); 
-		    				if (m > 0) {
-//		    					Particle.exchangeVelocities(p, q);
-		    					t -= m; 
-		    				} else {
-		    					break;
-		    				}
-		    			}
+		                float m = Particle.collision(p,q);
+		                if((m<1)&&(m>0)){
+		                    Particle.exchangeVelocities(p, q);
+		                }
 		    		}
 		    	}
 
 		    }
-		    
+
 		    // update
 //            p.velocity = Point.add(p.velocity, Particle.force(p, w));
             p.pos.add(p.velocity);
             p.pos.add(Particle.force(p, w));
 		    p.draw(c);
 		}
-		
+
 		if (++t >= 30 && particles.size() < MAX_PARTICLES) {
 			Random r = new Random();
 			particles.add(new Particle(curve, radius + pos.x + r.nextInt(300), radius + pos.x + pos.y + r.nextInt(300),  radius + pos.z + r.nextInt(300)));
 		}
-		
+
 	}
 
 	@Override
@@ -86,12 +81,12 @@ public class ParticleGenerator  implements Widget, MouseMotionListener  {
 
 	@Override
 	public void mouseMoved(float x, float y) {
-		
+
 	}
 
 	@Override
 	public void mouseClicked(float x, float y) {
-		
+
 	}
 
 }
